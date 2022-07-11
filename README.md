6b5a235b904f356feda9e22aeecd42de82de8cec
jiawei3@tal.com <jiawei3@tal.com>
2022-07-11 18:21:05 +0800

    update

---
+++ README.md
@@ -0,0 +1,26 @@
+# vue3-ts-template
+
+基于 @vue/cli typescript 项目模版 （vue create vue3-ts-template）
+
+## 环境
+
+node: v16.16.0
+react: ^18.2.0
+
+## Scripts
+
+### `npm install`
+
+装包
+
+### `npm run dev`
+
+[http://localhost:8080]
+
+### `npm run lint`
+
+Lints and fixes files
+
+### `npm run build`
+
+打包
---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    {{ title }}
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+  },
+  setup() {
+    const title = ref<string>('hello world');
+
+    onMounted(() => {
+      console.log(123);
+    });
+    
+    return {
+      title
+    };
+  }
+});
+</script>
