42b9e03ca5c3e2d42a8c21abacf300a1a88eb41a
jiawei3@tal.com <jiawei3@tal.com>
2022-08-05 10:00:37 +0800

    update

--- src/utils/sensors.ts
+++ src/utils/sensors.ts
@@ -1,3 +1,4 @@
+/** 神策埋点 */
 import sensors from 'sa-sdk-javascript';
 import queryString from 'query-string';
 import {
@@ -20,22 +21,6 @@ sensors.init({
 		scroll_notice_map: 'not_collect'
 	}
 });
-
-// // @ts-ignore
-// var sensors = window['sensorsDataAnalytic201505'];
-// sensors.init({
-// 	server_url: sensorsConfig[env],
-// 	is_track_single_page: true, // 单页面配置，默认开启，若页面中有锚点设计，需要将该配置删除，否则触发锚点会多触发 $pageview 事件
-// 	use_client_time: true,
-// 	send_type: 'beacon',
-// 	show_log: true,
-// 	heatmap: {
-// 		//是否开启点击图，default 表示开启，自动采集 $WebClick 事件，可以设置 'not_collect' 表示关闭。
-// 		clickmap: 'default',
-// 		//是否开启触达图，not_collect 表示关闭，不会自动采集 $WebStay 事件，可以设置 'default' 表示开启。
-// 		scroll_notice_map: 'not_collect'
-// 	}
-// });
 
 // 修改 H5 上报数据的 distinctID 
 const parsedHash: any = queryString.parse(location.hash);
